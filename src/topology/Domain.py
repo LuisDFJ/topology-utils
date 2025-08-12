0ea0384061c3b6ee338ac5eb1dde41d96a8e8615
LuisDFJ <luis.fuju@outlook.com>
2025-08-12 14:09:52 -0600

    Added Optimization Layer

--- src/topology/Domain.py
+++ src/topology/Domain.py
@@ -3,6 +3,9 @@ from src.topology.C1 import C1
 from src.topology.C2 import C2
 from src.topology.C3 import C3
 
+from src.topology.Opt import Opt
+from src.topology.Math import Math
+
 from typing import Callable
 
 class Domain:
@@ -22,29 +25,40 @@ class Domain:
             lambda x, dx = ( 0,0,0 ) : C3.map( N, D, *x, *dx ),
         )
 
-    def evalC1( self, u : int, v : int, w : int, d : int ):
-        p, q = C1.getC0( u,v,w,d )
-        P, Q = self.f( *self.M[0]( p ) ), self.f( *self.M[0]( q ) )
-        if ( P >= 0 ) != ( Q >= 0 ):
-            A,B,C,D = C1.getC3( u,v,w,d )
-            return (
-                self.M[3]( A ),
-                self.M[3]( B ),
-                self.M[3]( C ),
-                self.M[3]( D ),
-            )
-        return None
+        self.C1_record = {}
 
-    @classmethod
+    def evalC1( self, u : int, v : int, w : int, d : int ):
+        def wrapper( u : int, v : int, w : int, d : int ):
+            p, q = C1.getC0( u,v,w,d )
+            P, Q = self.f( *self.M[0]( p ) ), self.f( *self.M[0]( q ) )
+            if ( P >= 0 ) != ( Q >= 0 ):
+                return C1.getC3( u,v,w,d )
+            return None
+
+        if not (u,v,w,d) in self.C1_record.keys(): self.C1_record[ (u,v,w,d) ] = wrapper( u,v,w,d )
+        return self.C1_record[ (u,v,w,d) ]
+
+    def optC1( self, u : int, v : int, w : int, d : int ):
+        f  = lambda dx : self.f( *self.M[1]( (u,v,w,d), ( dx, ) ) )
+        dx = Opt.C1( 0, 1, f )
+        p  = self.M[1]( (u,v,w,d), ( dx, ) )
+        return p, Math.diffN( *p, self.f )
+
     def OptimizeSurface(
-        cls,
-        nodes : list[ tuple[ float, float, float ] ],
-        elements : list[ tuple[ int, ... ] ],
+        self,
+        nodes : list[ tuple[ int, int, int ] ]
     ):
-        pass
+        opt_nodes = []
+        keys = self.C1_record.keys()
+        for node in nodes:
+            pivots = []
+            for c1 in C3.getC1( *node ):
+                if c1 in keys: pivots.append( self.optC1( *c1 ) )
+            #opt_nodes.append( self.M[3]( node ) )
+        return opt_nodes
 
     def DualContour2D( self ):
-        nodes = []
+        nodes    = []
         elements = []
         for c1 in C1.iter( self.N ):
             facet = self.evalC1( *c1 )
@@ -54,7 +68,7 @@ class Domain:
                     if not n in nodes: nodes.append( n )
                     element.append( nodes.index( n ) )
                 elements.append( tuple( element ) )
-        return nodes, elements
+        return self.OptimizeSurface( nodes ), elements
 
 
 
---
+++ src/topology/Math.py
@@ -0,0 +1,36 @@
+from typing import Callable
+import math
+
+class Math:
+
+    H = 1e-5
+
+    @classmethod
+    def diff(
+        cls,
+        x : float, y : float, z : float,
+        f : Callable[ [float, float, float], float ]
+    ):
+        return (
+            f( x + Math.H, y, z ) - f( x - Math.H, y, z ),
+            f( x, y + Math.H, z ) - f( x, y - Math.H, z ),
+            f( x, y, z + Math.H ) - f( x, y, z - Math.H ),
+        )
+
+    @classmethod
+    def normal(
+        cls,
+        x : float, y : float, z : float
+    ):
+        n = math.sqrt( x**2 + y**2 + z**2 )
+        return x/n, y/n, z/n
+
+
+    @classmethod
+    def diffN(
+        cls,
+        x : float, y : float, z : float,
+        f : Callable[ [float, float, float], float ]
+    ):
+        return Math.normal( *Math.diff( x,y,z,f ) )
+
