27e70248740fe27f7d77293d17fbf77cef6fe7c8
LuisDFJ <luis.fuju@outlook.com>
2025-08-14 18:57:02 -0600

    Optimization done in parallelized version.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from src.dualcontour.DCParallel import DCParallel
 from src.utils.WavefrontParser import WavefrontParser
 
 from math import sin,cos,pi
-f = lambda w: lambda x,y,z: sin(w*x)*cos(w*y) + sin(w*y)*cos(w*z) + sin(w*z)*cos(w*x)
+def f( x,y,z ):
+    w = pi
+    return sin(w*x)*cos(w*y) + sin(w*y)*cos(w*z) + sin(w*z)*cos(w*x)
 #f = lambda x,y,z : x**2 + y**2 + z**2 - 0.9
 
 
-domain = DCParallel( f(pi) , ( -1.0,1.0,-1.0,1.0,-1.0,1.0 ), (40,40,40) )
+domain = DCParallel( f , ( -1.0,1.0,-1.0,1.0,-1.0,1.0 ), (40,40,40) )
 nodes, elements = domain.DualContour2D()
 WavefrontParser.parse( "mesh.obj", nodes, elements )
--- src/dualcontour/DCParallel.py
+++ src/dualcontour/DCParallel.py
@@ -1,12 +1,73 @@
-from ctypes import Array
-import ctypes
 from src.topology.C1 import C1
+from src.topology.C3 import C3
+from src.topology.Opt import Opt
+from src.topology.Math import Math
 from src.dualcontour.DCGeneric import DCGeneric
 
 from typing import Callable
-from multiprocessing import Process, RawArray, cpu_count
+from multiprocessing import Pool, Queue, cpu_count
+from functools import partial
 
 CPU = cpu_count()
+
+def evalC1(
+    u : int, v : int, w : int, d : int,
+    f: Callable[[float, float, float], float],
+    D: tuple[float, float, float, float, float, float],
+    N: tuple[int, int, int]
+):
+    if C1.bool( N, D, u,v,w,d, f ):
+        return C1.getC3( u,v,w,d )
+    return None
+
+def task(
+    u : int, v : int, w : int, d : int,
+    f: Callable[[float, float, float], float],
+    D: tuple[float, float, float, float, float, float],
+    N: tuple[int, int, int],
+    queue : Queue
+):
+    facet = evalC1( u,v,w,d,f,D,N )
+    if not isinstance( facet, type(None) ):
+        queue.put( facet )
+
+
+
+def optC1(
+    u : int, v : int, w : int, d : int,
+    f: Callable[[float, float, float], float],
+    D: tuple[float, float, float, float, float, float],
+    N: tuple[int, int, int]
+):
+    g  = lambda dx : f( *C1.map( N,D,u,v,w,d,dx ) ) 
+    dx = Opt.C1( -0.5, 0.5, g )
+    p  = C1.map( N,D,u,v,w,d,dx )
+    return p, Math.diffN( *p, f )
+
+def optC3(
+    u : int, v : int, w : int,
+    f: Callable[[float, float, float], float],
+    D: tuple[float, float, float, float, float, float],
+    N: tuple[int, int, int]
+):
+    A = []; b = []
+    for c1 in C3.getC1( u,v,w ):
+        if C1.bool( N, D, *c1, f ):
+            p, n = optC1( *c1, f, D, N )
+            A.append( n ); b.append( Math.dot( *n, *p ) )
+    x, rank = Opt.C3( A, b )
+    if rank: 
+        return x
+    else:
+        #L = self.L
+        #A = A + [ (L,0,0), (0,L,0), (0,0,L) ]
+        #b = b + list( self.M[3]( (u,v,w) ) )
+        #x, rank = Opt.C3( A, b )
+        #if rank:
+        #    return x
+        #else:
+            #return Math.clampCircular( x, self.M[3]( (u,v,w) ), self.r )
+        return C3.map( N,D,u,v,w )
 
 class DCParallel( DCGeneric ):
     def __init__(self,
@@ -15,46 +76,18 @@ class DCParallel( DCGeneric ):
         N: tuple[int, int, int] = (10,10,10) ) -> None:
         super().__init__(f, D, N)
 
-    def OptimizeSurfaceTask(
-        self,
-        nodes : list[ tuple[ int, int, int ] ],
-        idx0 : int, idxf : int,
-        array : tuple[ Array, Array, Array ] ,
-        cpu : int
-    ):
-        print( f"Running Task ({cpu})" )
-        for i in range( idx0, idxf ):
-            x,y,z = self.optC3( *nodes[ i ] )
-            array[0][i] = x; array[1][i] = y; array[2][i] = z;
-        print( f"Finishing Task ({cpu})" )
-
     def OptimizeSurface(
         self,
         nodes : list[ tuple[ int, int, int ] ]
     ):
-        N = len( nodes ); N_CPU = N // CPU; R_CPU = N % CPU
-        idxf = N_CPU + R_CPU; idx0 = 0
-        shared_memory = (
-            RawArray( ctypes.c_double, N ),
-            RawArray( ctypes.c_double, N ),
-            RawArray( ctypes.c_double, N )
-        )
-        pool = []
-        for cpu in range( CPU ):
-            pool.append(
-                Process( target=self.OptimizeSurfaceTask, name=f"Subprocess {cpu}/{CPU}", args=( nodes, idx0, idxf, shared_memory, cpu ) )
-            )
-            idx0  = idxf; idxf += N_CPU
+        with Pool( processes=cpu_count() ) as pool:
+            opt_nodes = pool.starmap( partial( optC3, f = self.f, D = self.D, N = self.N ), nodes  )
+        return opt_nodes
 
-        for p in pool: p.start()
-        for p in pool: p.join()
-
-        return [ ( x,y,z ) for x,y,z in zip( *shared_memory ) ]
-
-    def DualContour2D( self ):
+    def DualContour2D2( self ):
         nodes    = []
         elements = []
-        for c1 in C1.iter( self.N ):
+        for c1 in C1.iter( self.N ):    
             facet = self.evalC1( *c1 )
             if not isinstance( facet, type( None ) ):
                 element = []
@@ -64,5 +97,15 @@ class DCParallel( DCGeneric ):
                 elements.append( tuple( element ) )
         return self.OptimizeSurface( nodes ), elements
 
-
-
+    def DualContour2D( self ):
+        nodes    = []
+        elements = []
+        for c1 in C1.iter( self.N ):    
+            facet = self.evalC1( *c1 )
+            if not isinstance( facet, type( None ) ):
+                element = []
+                for n in facet:
+                    if not n in nodes: nodes.append( n )
+                    element.append( nodes.index( n ) )
+                elements.append( tuple( element ) )
+        return self.OptimizeSurface( nodes ), elements
